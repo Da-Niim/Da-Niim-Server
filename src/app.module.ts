60e2c1709ee08422797ba2e2cf6c139f01ac89fb
jinsy731 <jinsy731@gmail.com>
2023-11-15 23:52:06 +0900

    DAN-15/Chore: MongooseModule 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from "@nestjs/common"
+import { ConfigModule, ConfigService } from "@nestjs/config"
 import { MongooseModule } from "@nestjs/mongoose"
 import { MongoMemoryServer } from "mongodb-memory-server"
 import { AppController } from "./app.controller"
@@ -7,14 +8,25 @@ import { Sample, SampleSchema } from "./sample.model"
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.${process.env.NODE_ENV}.env`],
+    }),
     MongooseModule.forRootAsync({
-      useFactory: async () => {
-        const mongod = await MongoMemoryServer.create() // new MongoMemoryServer()는 에러 발생함
-        const mongoUri = mongod.getUri()
+      useFactory: async (configService: ConfigService) => {
+        let mongoUri: string
+        if (process.env.NODE_ENV === "dev") {
+          mongoUri = configService.get<string>("MONGO_URI")
+        } else {
+          const mongod = await MongoMemoryServer.create() // new MongoMemoryServer()는 에러 발생함
+          mongoUri = mongod.getUri()
+        }
+        console.log(`Connected to ${mongoUri}!`)
         return {
           uri: mongoUri,
         }
       },
+      inject: [ConfigService],
     }),
     MongooseModule.forFeature([{ name: Sample.name, schema: SampleSchema }]),
   ],
