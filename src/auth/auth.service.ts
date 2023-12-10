fb991702cb0874a9d326affef492a310764e224d
Lee MinGi <mingi3442@gmail.com>
2023-12-10 21:45:31 +0900

    DAN-19/Refactor: repository 추가 및 코드 리팩토링

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,25 +1,16 @@
 import { Injectable } from "@nestjs/common"
 import { ConfigService } from "@nestjs/config"
-
-import { UserService } from "src/user/user.service"
 import * as bcrypt from "bcrypt"
-import { UserRegisterDto } from "./dto/user-register.dto"
+
 @Injectable()
 export class AuthService {
-  constructor(
-    private readonly usersService: UserService,
-    private readonly configService: ConfigService,
-  ) {}
-  async registerUser(user: UserRegisterDto) {
-    const hashedPassword = await bcrypt.hash(
-      user.password,
-      this.configService.get<string>("SALT_ROUND"),
+  constructor(private readonly configService: ConfigService) {}
+  async hashingPassword(password: string) {
+    const hashedPassword = bcrypt.hash(
+      password,
+      parseInt(this.configService.get<string>("HASH_ROUNDS")),
     )
 
-    const newUser = await this.usersService.createUser({
-      ...user,
-      password: hashedPassword,
-    })
-    return newUser
+    return hashedPassword
   }
 }
--- src/user/dto/user-register.dto.ts
+++ src/user/dto/user-register.dto.ts
@@ -1,5 +1,5 @@
 import { PickType } from "@nestjs/swagger"
-import { User } from "../../user/entity/user.entity"
+import { User } from "../entity/user.entity"
 
 export class UserRegisterDto extends PickType(User, [
   "userId",
