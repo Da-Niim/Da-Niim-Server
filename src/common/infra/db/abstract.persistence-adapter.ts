53720560197b99d7d2abf04965c6b45061432581
jinsy731 <jinsy731@gmail.com>
2024-06-04 23:12:04 +0900

    ♻️ Abstract persistence adapter

--- src/common/infra/db/abstract.persistence-adapter.ts
+++ src/common/infra/db/abstract.persistence-adapter.ts
@@ -1,13 +1,31 @@
 import { Types } from "mongoose"
 import { AbstractRepository } from "src/common/abstract.repository"
 import { AbstractDocument } from "src/common/abstract.schema"
+import { BaseEntityMapper } from "./interface.entity-mapper"
 
 export abstract class AbstractPersistenceAdapter<
   T,
   D extends AbstractDocument,
   R extends AbstractRepository<D>,
+  M extends BaseEntityMapper<D, T>,
 > {
-  constructor(private readonly repository: R) {}
+  constructor(
+    protected readonly repository: R,
+    protected mapper: M,
+    private dbEntityConstructor: new (...args: any[]) => D,
+  ) {}
 
-  getById(id: Types.ObjectId): Promise<T> {}
+  async getById(id: Types.ObjectId): Promise<T> {
+    const result = new this.dbEntityConstructor(
+      await this.repository.getOne({ _id: id }),
+    )
+    return this.mapper.toDomainEntity(result)
+  }
+
+  async save(domain: T): Promise<T> {
+    const savedDBEntity = await this.repository.create(
+      new this.dbEntityConstructor(domain),
+    )
+    return this.mapper.toDomainEntity(savedDBEntity)
+  }
 }
